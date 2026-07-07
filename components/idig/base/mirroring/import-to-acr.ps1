fac5171e28027b911e3180a66961bbbaa50cc70c
kjellmlarsson <kjell.m.larsson@se.ibm.com>
2026-07-07 17:02:53 +0200

    chore(idig): add ACR import script and containerd mirror daemonset

---
+++ components/idig/base/mirroring/import-to-acr.ps1
@@ -0,0 +1,18 @@
+az acr import --name <acr>
+    --source mcr.microsoft.com/cbl-mariner/busybox:2.0 `
+    --image utils/busybox:2.0
+
+az acr import --name <acr> `
+  --source cp.icr.io/cp/datapower/datapower-nano-management:1.0.0 `
+  --image  cp-mirror/cp/datapower/datapower-nano-management:1.0.0 `
+  --username cp --password ibm-entitlement-key-pw
+
+az acr import --name <acr> `
+  --source cp.icr.io/cp/datapower/datapower-nano-runtime:1.0.0 `
+  --image  cp-mirror/cp/datapower/datapower-nano-runtime:1.0.0 `
+  --username cp --password ibm-entitlement-key-pw
+
+az acr import --name <acr> `
+  --source cp.icr.io/cp/datapower/datapower-nano-operator:1.0.0 `
+  --image  cp-mirror/cp/datapower/datapower-nano-operator:1.0.0 `
+  --username cp --password ibm-entitlement-key-pw
